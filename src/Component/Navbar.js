837804773470128066d42b538b9f3c441b2b2c63
Vayras <89837102+Vayras@users.noreply.github.com>
2023-09-26 18:05:57 +0530

    sidebar issue is fixed

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,84 +1,82 @@
-
-import {IoReorderThree} from 'react-icons/io5'
-import {BsSearch} from 'react-icons/bs'
-
-
+import React, { useState } from 'react'; // Import React and useState
+import { IoReorderThree } from 'react-icons/io5'
+import { BsSearch } from 'react-icons/bs'
 import logo from '../image/logo.png'
 import '../style/Navbar.css'
 import { Link } from 'react-router-dom'
+
 function Navbar() {
+  const [showSideNav, setShowSideNav] = useState(false); // Initialize showSideNav state
 
-function handleClick(e){
-alert("Something is wrong?")
-}
-
- 
- 
-
- 
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('hello world');
+    setShowSideNav(!showSideNav); // Toggle the state when the button is clicked
+  }
 
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand sdbar-logo" href="#"  id='logo'><IoReorderThree/></a>
-   <a href='#'>
-    <img class="navbar-brand logo-img" src={logo} alt='this'/></a>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 navbar-content">
-      <li class="nav-item">
-          <a class="nav-link active" aria-current="page" >
-            <Link to="/" className='Link  Bookmark'>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button className="navbar-toggler" onClick={handleClick} type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <a onClick={handleClick} className="navbar-brand sdbar-logo" href="#" id='logo'><IoReorderThree /></a>
+          <a href='#'>
+            <img className="navbar-brand logo-img" src={logo} alt='this' /></a>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-content">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" >
+                  <Link to="/" className='Link  Bookmark'>
+                    Home
+                  </Link>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" >
+                  <Link to="/bookmark" className='Link  Bookmark'>
+                    BookMark
+                  </Link>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" >
+                  <Link to="/about" className='Link About'>
+                    About us
+                  </Link>
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2 search-box" type="search" placeholder="Search..." aria-label="Search" />
+              <button
+                className="btn btn-search btn-outline-success"
+                type="button"
+                onClick={(e) => handleClick(e)}
+              ></button>
+            </form>
+          </div>
+        </div>
+      </nav>
 
-       Home
-            </Link>
-             </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" >
-            <Link to="/bookmark" className='Link  Bookmark'>
-
-          BookMark
-            </Link>
-             </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" >
-          <Link to="/about" className='Link About'>
-
-          About us
-  </Link>
-            </a>
-        </li>
-        
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2 search-box" type="search" placeholder="Search..." aria-label="Search" />
-        <button class="btn btn-search btn-outline-success" type="submit" onClick={handleClick}><BsSearch/></button>
-      </form>
-    </div>
-  </div>
-</nav>
-<section>
-  <div className="sidebar" id='sidebar'>
-<div className="sidebar-content b-1">home-icon-bookmark</div>
-<div className="sidebar-content b-2">All</div>
-<div className="sidebar-content b-3">Remote Jobs</div>
-<div className="sidebar-content b-4">AL</div>
-<div className="sidebar-content b-5">AL</div>
-<div className="sidebar-content b-6">AL</div>
-<div className="sidebar-content b-7">AL</div>
-<div className="sidebar-content b-8">AL</div>
-<div className="sidebar-content b-9">AL</div>
-<div className="sidebar-content b-10">UI</div>
-<div className="sidebar-content b-11">UI</div>
-<div className="sidebar-content b-12">UI</div>
-  </div>
-</section>
-
+      {/* Conditionally render the side navigation based on the showSideNav state */}
+      {showSideNav && (
+        <div className="sidebar">
+          <div className="sidebar-content ">home-icon-bookmark</div>
+          <div className="sidebar-content ">All</div>
+          <div className="sidebar-content ">Remote Jobs</div>
+          <div className="sidebar-content ">AL</div>
+          <div className="sidebar-content ">AL</div>
+          <div className="sidebar-content">AL</div>
+          <div className="sidebar-content ">AL</div>
+          <div className="sidebar-content ">AL</div>
+          <div className="sidebar-content ">AL</div>
+          <div className="sidebar-content ">UI</div>
+          <div className="sidebar-content ">UI</div>
+          <div className="sidebar-content ">UI</div>
+        </div>
+      )}
     </div>
   )
 }
