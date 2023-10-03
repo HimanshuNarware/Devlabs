d45bc8f350652d7e98fa393d893eb714585f9af8
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-10-03 12:08:35 +0530

    Fixed The Twitter logo icon

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import logo from '../image/logo.png'
 import '../style/Footer.css'
-import {FaLinkedinIn, FaTwitter} from 'react-icons/fa'
+import {FaLinkedinIn} from 'react-icons/fa'
+import {FaXTwitter} from 'react-icons/fa6'
 import {BsGithub}from 'react-icons/bs'
 import {AiTwotoneMail}from 'react-icons/ai'
 function Footer() {
@@ -15,7 +16,7 @@ function Footer() {
     <h4 class="card-title ">Contact us</h4>
     <div className='contact-link'>
     <div className='cntc-link'><a href='https://www.linkedin.com/in/himanshunarware/' target='_blank' ><FaLinkedinIn/></a></div>  
-  <div className='cntc-link'><a href='https://twitter.com/N_Himanshu_\' target='_blank' >   <FaTwitter/></a></div>  
+  <div className='cntc-link'><a href='https://twitter.com/N_Himanshu_\' target='_blank' >   <FaXTwitter/></a></div>  
   <div className='cntc-link'><a href='https://github.com/HimanshuNarware' target='_blank' >   <BsGithub/></a></div>  
   <div className='cntc-link'><a href='https://twitter.com/_Jason_Dsouza' target='_blank' >   <AiTwotoneMail/></a></div> 
     </div>
