1546c825767eb1f6d0ab2e8a4160f126986f4eee
Hrid Chakraborty <hridchakraborty@gmail.com>
2023-10-17 10:02:56 +0530

    Replaced Repetitive code in NavbarCenter with a NavbarItem Component

--- src/Component/Navbar/NavbarCenter.js
+++ src/Component/Navbar/NavbarCenter.js
@@ -1,31 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../../style/Navbar.css";
+import NavbarItem from "./NavbarItem";
 
 function NavbarCenter(){
   return (
     <span className="navbar-center">
       <ul className="mb-2 navbar-content">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page">
-            <Link to="/" className="Link  Bookmark">
-              Home
-            </Link>
-          </a>
+          <NavbarItem description="Home" to="/" />
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page">
-            <Link to="/bookmark" className="Link  Bookmark">
-              BookMark
-            </Link>
-          </a>
+          <NavbarItem description="BookMark" to="/bookmark" />
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page">
-            <Link to="/about" className="Link About">
-              About us
-            </Link>
-          </a>
+          <NavbarItem description="About Us" to="/about" />
         </li>
       </ul>
     </span>
