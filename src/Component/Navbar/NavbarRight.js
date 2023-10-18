b9b178de575c5943fbc982c885b018f5c178abe3
Bhumika Bachchan <bhumikabachchan11315@gmail.com>
2023-10-18 11:53:10 +0530

    Search ui updated

--- src/Component/Navbar/NavbarRight.js
+++ src/Component/Navbar/NavbarRight.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import "../../style/Navbar.css"
 import { BsSearch } from "react-icons/bs";
 
 function NavbarRight(props) {
-  function handleSearch(e) {
-    e.preventDefault();
-  }
-  const handleInputChange = (e) => {
-    props.setSearchQuery(e.target.value);
-  };
+  // function handleSearch(e) {
+  //   e.preventDefault();
+  // }
+  // const handleInputChange = (e) => {
+  //   props.setSearchQuery(e.target.value);
+  // };
+  const [search, setSearch] = useState("Search...");
 
   return (
     <div className="navbar-right" id="navbarTogglerDemo03">
-      <form className="d-flex" role="search">
+      {/* <form className="d-flex" role="search">
         <input
           className="form-control me-2 search-box"
           type="search"
@@ -26,6 +27,24 @@ function NavbarRight(props) {
           onClick={(e) => handleSearch(e)}
         >
           <BsSearch />
+        </button>
+      </form> */}
+      <form role="search" className="search-bar">
+        <input
+          type="search"
+          name="search"
+          pattern=".*\S.*"
+          required
+          // value={search}
+          // values={values.keyword}
+          onChange={(e)=>setSearch(e.target.value)}
+        />
+        <button
+          className="search-btn"
+          type="submit"
+          // onClick={(e) => handleSearch(e)}
+        >
+          <span>Search</span>
         </button>
       </form>
     </div>
