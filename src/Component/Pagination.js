fd8588b330dabb939cda4c462466054368c03ec2
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-10-22 13:07:41 +0200

    moving style to separate css file

--- src/Component/Pagination.js
+++ src/Component/Pagination.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../style/Pagination.css"
 
 const Pagination = ({ firstPostIndex, lastPostIndex, currentPost1, prePage, numbers, currentPage, changeCPage, nextPage  }) => {
   
