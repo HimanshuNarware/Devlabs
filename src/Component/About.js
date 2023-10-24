b76b96fcb752c944b675781c338bfcdaa8073871
Ankit Tyagi <ankit.tyagi555@gmail.com>
2023-10-24 13:48:28 +0530

    Improve the UI of the about section

--- src/Component/About.js
+++ src/Component/About.js
@@ -33,33 +33,33 @@ function About() {
 
   	return (
     	<div>
-      		<div className='container-one'>
-        		<div className='bx-1'>
-          			<p>
-            			Devlabs is an application that is used to search for free tools that are both free and helpful for our needs. It is built by the amazing open-source community.
-          			</p>
-          			<button className='repo-btn'> <a href="https://github.com/HimanshuNarware/Devlabs" target="_blank">Star the repo &#9733;</a></button>
-        		</div>
-        		<div className='bx-2'>
-          			<img className="img " src='https://imgs.search.brave.com/QKiV2kjLLbCo5yYmlXGywHNk3CbfEVQPXvl7zoMhR3s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9vcGVu/c291cmNlLmd1aWRl/L2Fzc2V0cy9pbWFn/ZXMvaWxsb3MvYmFs/YW5jZS5zdmc.svg' alt='img' />
-        		</div>
-      		</div>
+				<div className='container-one'>
+					<div className='bx-1'>
+						<img className="img " src='https://imgs.search.brave.com/QKiV2kjLLbCo5yYmlXGywHNk3CbfEVQPXvl7zoMhR3s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9vcGVu/c291cmNlLmd1aWRl/L2Fzc2V0cy9pbWFn/ZXMvaWxsb3MvYmFs/YW5jZS5zdmc.svg' alt='img' />
+					</div>
+					<div className='bx-2'>
+						<p>
+							Devlabs is an application that is used to search for free tools that are both free and helpful for our needs. It is built by the amazing open-source community.
+						</p>
+						<button className='repo-btn'> <a href="https://github.com/HimanshuNarware/Devlabs" target="_blank">Star the repo &#9733;</a></button>
+					</div>
+				</div>
 
-      		<div className='container-one'>
-        		<div className='bx-1'> 
-          			<img className="img" src='https://imgs.search.brave.com/pQigBw5L8ODloy7loco05dRT56xI3iAmVRKPCUYYltc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvOTYw/NTMxMDEyL3ZlY3Rv/ci9vcGVuLXNvdXJj/ZS1pY29uLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1SYUxi/UU1fMjA2bHNHcHRZ/WW1ZUG0zYk42SHVM/WFZlZWFtb3ozZXNL/S0VRPQ' alt='img'/>
-        		</div>
-        		<div className='bx-2'>
-          			<p>
-            			Great things are never created in isolation. Thanks to our amazing Contributors, we've brought this product to life. And with your help, we can continue to make it even better. If you're a Developer or a Tech enthusiast, you can help us create a better experience for everyone. We are excited to hear your thoughts and ideas.
-          			</p>
-          			<button className='repo-btn'>
-            			<a href="https://github.com/HimanshuNarware" target="_blank">
-              				Learn more ➜
-            			</a>
-          			</button>
-        		</div>
-      		</div>
+				<div className='container-two'>
+					<div className='bx-1'>
+						<img className="img" src='https://imgs.search.brave.com/pQigBw5L8ODloy7loco05dRT56xI3iAmVRKPCUYYltc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvOTYw/NTMxMDEyL3ZlY3Rv/ci9vcGVuLXNvdXJj/ZS1pY29uLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1SYUxi/UU1fMjA2bHNHcHRZ/WW1ZUG0zYk42SHVM/WFZlZWFtb3ozZXNL/S0VRPQ' alt='img' />
+					</div>
+					<div className='bx-2'>
+						<p>
+							Great things are never created in isolation. Thanks to our amazing Contributors, we've brought this product to life. And with your help, we can continue to make it even better. If you're a Developer or a Tech enthusiast, you can help us create a better experience for everyone. We are excited to hear your thoughts and ideas.
+						</p>
+						<button className='repo-btn'>
+							<a href="https://github.com/HimanshuNarware" target="_blank">
+								Learn more ➜
+							</a>
+						</button>
+					</div>
+				</div>
 
       		<div className='contributor-container'>
         		<h1>Founder</h1>
@@ -87,8 +87,8 @@ function About() {
             			return (
               				<div className='content-box' key={contributor.id}>
                 				<img className='logo' src={contributor.avatar_url} alt="avatar"/>
-                				<h3>{contributor.login}</h3>
-                				<h4>{contributor.contributions} commits</h4>
+                				<h5>{contributor.login}</h5>
+                				<h6>{contributor.contributions} commits</h6>
                 				<button className='profile-btn'>
                 					<a href={contributor.html_url} target="_blank">
                     					Profile
