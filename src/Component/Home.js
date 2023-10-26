2d4ee21ce9ed2651975a355cc4c5fece6f5d168f
Mayank77maruti <125661248+Mayank77maruti@users.noreply.github.com>
2023-10-27 02:22:10 +0530

    Merged Conflict

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,19 +3,20 @@ import dataBaseData from "./../DB/product.json";
 import "../style/Home.css";
 import { setSource } from "../Slice/DataSlice";
 import { useDispatch } from "react-redux";
+import Pagination from "./Pagination";
+import { getPaginationData, changePage } from "../utils/paginationData";
+
+const CARDS_PER_PAGE = 16
 
 function Home(props) {
   const [localStorageValue, setLocalStorageValue] = useState(
     localStorage.getItem("filter") || ""
   );
-  let itemList = "";
-  const [currentPage, setCurrentPage] = useState(1);
-  const postPerpage = 16;
-  const lastPostIndex = currentPage * postPerpage;
-  const firstPostIndex = lastPostIndex - postPerpage;
+  const [currentPage, setCurrentPage] = useState(1); 
 
-  const currentPost1 = dataBaseData;
+  const data = dataBaseData;
   let allvalue = dataBaseData;
+
   useEffect(() => {
     const handleStorageChange = () => {
       setLocalStorageValue(localStorage.getItem("filter"));
@@ -25,102 +26,77 @@ function Home(props) {
       window.removeEventListener("storage", handleStorageChange);
     };
   }, []);
+
   if (localStorageValue === "undefined" || localStorageValue === "all") {
     allvalue = dataBaseData;
     console.log("No localstorage item");
   } else if (localStorageValue !== "all" && localStorageValue !== "undefined") {
     console.log(localStorageValue);
     if (localStorage.getItem("filter-2")) {
-      allvalue = currentPost1.filter(
+      allvalue = data.filter(
         (e) =>
           e.category.toLowerCase().includes(localStorageValue) ||
           e.category.includes(localStorage.getItem("filter-2"))
       );
     } else {
-      allvalue = currentPost1.filter((e) =>
+      allvalue = data.filter((e) =>
         e.category.toLowerCase().includes(localStorageValue)
       );
     }
     console.log(allvalue);
   }
 
+  // filters data from searchbar query 
   const filteredData = allvalue.filter((datalist) => {
     return (
-      datalist.productName.toLowerCase().includes(props.searchQuery.toLowerCase()) ||
-      datalist.description.toLowerCase().includes(props.searchQuery.toLowerCase())
+      datalist.productName
+        .toLowerCase()
+        .includes(props.searchQuery.toLowerCase()) ||
+      datalist.description
+        .toLowerCase()
+        .includes(props.searchQuery.toLowerCase())
     );
   });
-  const currentPost = filteredData.slice(firstPostIndex, lastPostIndex);
-  const npage = Math.ceil(filteredData.length / postPerpage);
-  const numbers = [...Array(npage + 1).keys()].slice(1);
+
+  const paginationValues = getPaginationData(currentPage, CARDS_PER_PAGE, filteredData)
+  const { lastCardIndex, firstCardIndex, allPagesNumbers, currentPageData} = paginationValues
+
+  const handlePageChange = (value) => {
+    changePage(value, currentPage, setCurrentPage)
+  }
+
   const dispatch = useDispatch();
-
-  function prePage() {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  }
-
-  function nextPage() {
-    if (currentPage < npage) {
-      setCurrentPage(currentPage + 1);
-    }
-  }
-
-  function changeCPage(id) {
-    setCurrentPage(id);
-  }
 
   return (
     <div className="page-container">
       <div className="main-container">
-        {(itemList = currentPost.map((datalist) => {
-          return (
-            <div className="content-box-home" key={datalist.id}>
-              <img className="logo" src={datalist.image} alt={datalist.category} />
-              <h2>{datalist.productName}</h2>
-              <p className="content-box-text">{datalist.description}</p>
-              <button className="btn-b-box" onClick={() => window.open(datalist.link)}>
-                Link
-              </button>
-              <button
-                className="btn-b-box"
-                onClick={() => {
-                  const bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
-                  if (bookmarks === null) {
-                    localStorage.setItem(
-                      "bookmarks",
-                      JSON.stringify([
-                        {
-                          image: datalist.image,
-                          name: datalist.productName,
-                          desc: datalist.description,
-                          link: datalist.link,
-                        },
-                      ])
+        {
+          (currentPageData?.map((datalist) => {
+            return (
+              <div className="content-box-home" key={datalist.id}>
+                <img
+                  className="logo"
+                  src={datalist.image}
+                  alt={datalist.category}
+                />
+                <h2>{datalist.productName}</h2>
+                <p className="content-box-text">{datalist.description}</p>
+                <button
+                  className="btn-b-box"
+                  onClick={() => window.open(datalist.link)}
+                >
+                  Link
+                </button>
+                <button
+                  className="btn-b-box"
+                  onClick={() => {
+                    const bookmarks = JSON.parse(
+                      localStorage.getItem("bookmarks")
                     );
-                    dispatch(
-                      setSource({
-                        image: datalist.image,
-                        name: datalist.productName,
-                        desc: datalist.description,
-                        link: datalist.link,
-                      })
-                    );
-                  } else {
-                    let found = false;
-                    for (let item of bookmarks) {
-                      if (item.name === datalist.productName) {
-                        found = true;
-                        break;
-                      }
-                    }
-
-                    if (!found) {
+                    if (bookmarks === null) {
                       localStorage.setItem(
                         "bookmarks",
                         JSON.stringify([
-                          ...bookmarks,
                           {
                             image: datalist.image,
                             name: datalist.productName,
@@ -137,41 +113,57 @@ function Home(props) {
                           link: datalist.link,
                         })
                       );
+                    } else {
+                      let found = false;
+                      for (let item of bookmarks) {
+                        if (item.name === datalist.productName) {
+                          found = true;
+                          break;
+                        }
+                      }
+
+                      if (!found) {
+                        localStorage.setItem(
+                          "bookmarks",
+                          JSON.stringify([
+                            ...bookmarks,
+                            {
+                              image: datalist.image,
+                              name: datalist.productName,
+                              desc: datalist.description,
+                              link: datalist.link,
+                            },
+                          ])
+                        );
+                        dispatch(
+                          setSource({
+                            image: datalist.image,
+                            name: datalist.productName,
+                            desc: datalist.description,
+                            link: datalist.link,
+                          })
+                        );
+                      }
                     }
-                  }
-                }}
-              >
-                Bookmark
-              </button>
-            </div>
-          );
-        }))}
+                  }}
+                >
+                  Bookmark
+                </button>
+              </div>
+            );
+          })
+        )}
       </div>
-      {/* pagination */}
-      <nav>
-        <div className="page-index">
-          Showing {firstPostIndex + 1}-{lastPostIndex} from {currentPost1.length} results
-        </div>
-        <ul className="pagination">
-          <li className="page-item">
-            <a href="#" className="page-link" onClick={prePage}>
-              prev
-            </a>
-          </li>
-          {numbers.map((n, i) => (
-            <li className={`page-item ${currentPage === n ? "active" : ""}`} key={i}>
-              <a href="#" className="page-link" onClick={() => changeCPage(n)}>
-                {n}
-              </a>
-            </li>
-          ))}
-          <li className="page-item">
-            <a href="#" className="page-link" onClick={nextPage}>
-              next
-            </a>
-          </li>
-        </ul>
-      </nav>
+
+      <Pagination 
+        firstCardIndex={firstCardIndex}
+        lastCardIndex={lastCardIndex}
+        dataLength={data.length}
+        allPagesNumbers={allPagesNumbers} 
+        currentPage={currentPage} 
+        handlePageChange={handlePageChange}
+        scrollPosition={'top'}
+      />
     </div>
   );
 }
