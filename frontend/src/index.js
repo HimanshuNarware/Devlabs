ae05531fe88fa081982902e1ca1b826e24c1317c
Aman <dv451197@gmail.com>
2024-03-19 13:29:03 +0530

    fix images on darkmode active

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './Store/store'
-import Darkmode from 'darkmode-js';
-import { BrowserRouter } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./Store/store";
+import Darkmode from "darkmode-js";
+import { BrowserRouter } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const options = {
-  bottom: '64px',
-  right: 'unset',
-  left: '32px',
-  time: '0.5s',
-  mixColor: '#fff',
-  backgroundColor: '#101623',
-  buttonColorDark: '#f0f0f0',
-  buttonColorLight: '#100f2c',
+  bottom: "64px",
+  right: "unset",
+  left: "32px",
+  time: "0.5s",
+  // mixColor: "#fff",
+  backgroundColor: "#fff",
+  buttonColorDark: "#f0f0f0",
+  buttonColorLight: "#000",
   saveInCookies: true,
-  label: '🌓',
-  autoMatchOsTheme: true,
-}
+  label: "🌓",
+  autoMatchOsTheme: false,
+};
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
-
 
 root.render(
   <React.StrictMode>
