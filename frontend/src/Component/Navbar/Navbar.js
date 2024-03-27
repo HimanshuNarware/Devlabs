fbabf8365c7d0d5778e904a9c659f3140e8b4a48
Aman <dv451197@gmail.com>
2024-03-27 13:53:46 +0530

    installed tailwindcss

--- frontend/src/Component/Navbar/Navbar.js
+++ frontend/src/Component/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../style/Navbar.css"
+import "../../style/Navbar.css";
 import Sidebar from "../Sidebar/Sidebar";
 import NavbarLeft from "./NavbarLeft";
 import NavbarCenter from "./NavbarCenter";
@@ -9,16 +9,15 @@ function Navbar(props) {
   const [showSideNav, setShowSideNav] = useState(false);
 
   return (
-    <header >
-      <div className="navbar">
-        <NavbarLeft showSideNav={showSideNav} setShowSideNav={setShowSideNav}/>
+    <header>
+      <div className="navbar bg-black">
+        <NavbarLeft showSideNav={showSideNav} setShowSideNav={setShowSideNav} />
         <NavbarCenter />
-        <NavbarRight setSearchQuery={props.setSearchQuery} /> {/* Pass setSearchQuery directly */}
+        <NavbarRight setSearchQuery={props.setSearchQuery} />{" "}
+        {/* Pass setSearchQuery directly */}
       </div>
 
-      
-        <Sidebar showSideNav={showSideNav} />
-      
+      <Sidebar showSideNav={showSideNav} />
     </header>
   );
 }
