cad5f502b4e02238b57f63c5ba3173a4b28912e4
Aman <dv451197@gmail.com>
2024-03-27 13:44:58 +0530

    install tailwind css

--- frontend/src/Component/Toggle.js
+++ frontend/src/Component/Toggle.js
@@ -7,22 +7,29 @@ import { BsLightbulbOffFill, BsFillLightbulbFill } from "react-icons/bs";
 export const DarkModeToggle = () => {
   const { isDark, setIsDark } = useColorScheme();
   return (
-    <div className="flex items-center justify-center flex-row gap-2 relative cursor-pointer">
-      <div className="flex items-center justify-center flex-row  cursor-pointer">
-        {isDark ? (
-          <BsFillLightbulbFill className="text-amber-400 text-xl" />
-        ) : (
-          <BsLightbulbOffFill className="text-slate-800 text-xl" />
-        )}
-      </div>
-      <Toggle
-        checked={isDark}
-        onChange={({ target }) => setIsDark(target.checked)}
-        icons={false}
-        aria-label="Dark mode toggle"
-        className="absolute  opacity-0 w-8 cursor-pointer
-        "
-      />
-    </div>
+    //     <div className="flex items-center justify-center flex-row gap-2 relative cursor-pointer">
+    //       <div className="flex items-center justify-center flex-row  cursor-pointer">
+    //         {isDark ? (
+    //           <BsFillLightbulbFill className="text-amber-400 text-xl" />
+    //         ) : (
+    //           <BsLightbulbOffFill className="text-slate-800 text-xl" />
+    //         )}
+    //       </div>
+    //       <Toggle
+    //         checked={isDark}
+    //         onChange={({ target }) => setIsDark(target.checked)}
+    //         icons={false}
+    //         aria-label="Dark mode toggle"
+    //         className="absolute  opacity-0 w-8 cursor-pointer
+    //         "
+    //       />
+    //     </div>
+
+    <Toggle
+      checked={isDark}
+      onChange={({ target }) => setIsDark(target.checked)}
+      icons={{ checked: "🌙", unchecked: "🔆" }}
+      aria-label="Dark mode toggle"
+    />
   );
 };
