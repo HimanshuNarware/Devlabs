b0fac3e3fa6b8a3338fdde46ef770a032fd587dc
Dhaval Rathod <117587892+dhruv389@users.noreply.github.com>
2024-05-26 01:39:14 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,8 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(
   cors({
-    // origin: process.env.FRONTEND_URL.replace(/"/g, ""),
-    origin:"*",
+     origin: process.env.FRONTEND_URL.replace(/"/g, ""),
     credentials: true,
   })
 );
