6c65bdf6635594f3b77592f013774594ad16a423
Ankitluhar <ankitluhar5121@gmail.com>
2024-05-25 18:08:19 +0530

    Add new feature: enhancement the chat-bot feature

--- frontend/src/ChatAssistant/ChatAssistant.jsx
+++ frontend/src/ChatAssistant/ChatAssistant.jsx
@@ -1,29 +1,56 @@
 import Chatbot from "react-chatbot-kit";
-import 'react-chatbot-kit/build/main.css'
-import config from '../ChatBot/config.jsx';
-import MessageParser from '../ChatBot/MessageParser';
-import ActionProvider from '../ChatBot/ActionProvider';
-import "./ChatAssistant.css"
-import React, { useState } from 'react';
-import chatbotLogo from '../assets/chatbotLogo.jpeg'
+import "react-chatbot-kit/build/main.css";
+import config from "../ChatBot/config.jsx";
+import MessageParser from "../ChatBot/MessageParser";
+import ActionProvider from "../ChatBot/ActionProvider";
+import "./ChatAssistant.css";
+import React, { useState, useRef, useEffect } from "react";
+import chatbotLogo from "../assets/chatbotLogo.jpeg";
 
 const ChatAssistant = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const chatbotRef = useRef(null);
 
+  // Handling the outside click to close the chatbot
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (chatbotRef.current && !chatbotRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [chatbotRef]);
+
+  // Toggle chatbot visibility
   const toggleChatbot = () => {
     setIsOpen(!isOpen);
   };
-    return (
-      <div className="chatbot">
-        <img className="Logo" src={chatbotLogo} alt="Logo" onClick={toggleChatbot} />
-      {isOpen && 
+
+  return (
+    <div className="chatbot" ref={chatbotRef}>
+      {/* Render the chatbot component only if isOpen is true */}
+      {isOpen && (
         <Chatbot
           config={config}
           messageParser={MessageParser}
           actionProvider={ActionProvider}
-        />}
-      </div>
-    );
-  }
-  
+        />
+      )}
+      {/* Render the chatbot logo only if isOpen is false */}
+      {!isOpen && (
+        <img
+          className="Logo"
+          src={chatbotLogo}
+          alt="Logo"
+          onClick={toggleChatbot}
+        />
+      )}
+    </div>
+  );
+};
+
 export default ChatAssistant;
