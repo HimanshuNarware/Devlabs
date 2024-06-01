ec975f8bab121284f7dcb8023e02f07d9b164d6a
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-01 16:48:01 +0530

    added new pages

---
+++ frontend/src/pages/AI.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AI = () => {
+  return (
+    <div style={{marginTop: "200px"}}>AI</div>
+  )
+}
+
+export default AI
---
+++ frontend/src/pages/UI.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UI = () => {
+  return (
+    <div style={{marginTop: "200px"}}>UI</div>
+  )
+}
+
+export default UI
