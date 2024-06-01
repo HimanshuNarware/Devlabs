3edb8089412eb1441b2aa723a7407a5a2ce2692b
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-01 06:31:42 +0530

    resolve chat-bot error

--- frontend/src/ChatAssistant/ChatAssistant.jsx
+++ frontend/src/ChatAssistant/ChatAssistant.jsx
@@ -1,12 +1,12 @@
 import Chatbot from "react-chatbot-kit";
-import 'react-chatbot-kit/build/main.css'
-import config from '../ChatBot/config.jsx';
-import MessageParser from '../ChatBot/MessageParser';
-import ActionProvider from '../ChatBot/ActionProvider';
-import "./ChatAssistant.css"
-import React, { useState } from 'react';
-import chatbotLogo from '../assets/chatbotLogo.jpeg'
-import chatbotLogo1 from '../assets/logo1.png'
+import "react-chatbot-kit/build/main.css";
+import config from "../ChatBot/config.jsx";
+import MessageParser from "../ChatBot/MessageParser";
+import ActionProvider from "../ChatBot/ActionProvider";
+import "./ChatAssistant.css";
+import React, { useState } from "react";
+import chatbotLogo from "../assets/chatbotLogo.jpeg";
+import chatbotLogo1 from "../assets/logo1.png";
 
 const ChatAssistant = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,28 +14,33 @@ const ChatAssistant = () => {
   const toggleChatbot = () => {
     setIsOpen(!isOpen);
   };
-    return (
-      <div className="chatbot">
-        <img className="Logo" src={chatbotLogo} alt="Logo" onClick={toggleChatbot} 
-        onMouseEnter={() => (document.querySelector(".Logo").src = chatbotLogo1)} 
-        onMouseLeave={() => (document.querySelector(".Logo").src = chatbotLogo)}
-        />
-      {isOpen && 
-
-        <img className="Logo" src={chatbotLogo} alt="Logo" onClick={toggleChatbot} />
-        <div className={`${isOpen?"chatbot-animation":"chatbot-close-animation"}`}>
-
-       {isOpen && 
-        <Chatbot
-          config={config}
-          messageParser={MessageParser}
-          actionProvider={ActionProvider}
-          />
-          
+  return (
+    <div className="chatbot">
+      <img
+        className="Logo"
+        src={chatbotLogo}
+        alt="Logo"
+        onClick={toggleChatbot}
+        onMouseEnter={() =>
+          (document.querySelector(".Logo").src = chatbotLogo1)
         }
+        onMouseLeave={() => (document.querySelector(".Logo").src = chatbotLogo)}
+      />
+      {isOpen && (
+        <div
+          className={`${
+            isOpen ? "chatbot-animation" : "chatbot-close-animation"
+          }`}
+        >
+          <Chatbot
+            config={config}
+            messageParser={MessageParser}
+            actionProvider={ActionProvider}
+          />
         </div>
-      </div>
-    );
-  }
-  
+      )}
+    </div>
+  );
+};
+
 export default ChatAssistant;
