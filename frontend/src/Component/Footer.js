b065d2ee95b482f9a865de81b4d33a4db309b3dc
HARSH <sainiharsha001@gmail.com>
2024-06-09 17:07:23 +0530

    Fixes color and hover on social media icons

--- frontend/src/Component/Footer.js
+++ frontend/src/Component/Footer.js
@@ -11,216 +11,312 @@ import { SiGmail } from "react-icons/si";
 import { Link } from "react-router-dom";
 
 function Footer() {
-  return (
-    <footer>
+    return ( <
+        footer >
 
-      <div className='Footer'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-6 col-lg-4 col-12 ft-1'>
-              <a href='https://www.devlabsstore.tech/'>
-                <img width={209} src={logo} alt='devlaps' />
-              </a>
-              <h5>
-                Facing an issue or just <br />
-                enjoying the site?
-      <div className="Footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 col-lg-4 col-12 ft-1">
-              <a href="https://www.devlabsstore.tech/">
-                <img width={209} src={logo} alt="devlaps" />
-              </a>
-              <h5>
-                Facing an issue or just <br />
-                enjoying the site?{" "}
+        <
+        div className = 'Footer' >
+        <
+        div className = 'container' >
+        <
+        div className = 'row' >
+        <
+        div className = 'col-md-6 col-lg-4 col-12 ft-1' >
+        <
+        a href = 'https://www.devlabsstore.tech/' >
+        <
+        img width = { 209 }
+        src = { logo }
+        alt = 'devlaps' / >
+        <
+        /a> <
+        h5 >
+        Facing an issue or just < br / >
+        enjoying the site ?
+        <
+        div className = "Footer" >
+        <
+        div className = "container" >
+        <
+        div className = "row" >
+        <
+        div className = "col-md-6 col-lg-4 col-12 ft-1" >
+        <
+        a href = "https://www.devlabsstore.tech/" >
+        <
+        img width = { 209 }
+        src = { logo }
+        alt = "devlaps" / >
+        <
+        /a> <
+        h5 >
+        Facing an issue or just < br / >
+        enjoying the site ? { " " }
 
-              </h5>
-              <button>
-                <Link to="/review">Tell us now</Link>
-              </button>
-            </div>
-            <div className="col-md-6 col-lg-3 col-12 ft-2">
-              <h5>Company</h5>
-              <ul>
-                <li className="nav-item">
-                  <a className="" href="https://www.devlabsstore.tech/">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="" href="https://www.devlabsstore.tech/about">
-                    About Us
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="" href="https://www.devlabsstore.tech/bookmark">
-                    Bookmark
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="" href="https://www.linkedin.com/in/himanshunarware/">
-                    Contact
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="" href="https://www.devlabsstore.tech/open-source">
-                    Open Source
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-6 col-lg-3 col-12 ft-2">
-              <h5>Quick Links</h5>
-              <ul>
+        <
+        /h5> <
+        button >
+        <
+        Link to = "/review" > Tell us now < /Link> <
+        /button> <
+        /div> <
+        div className = "col-md-6 col-lg-3 col-12 ft-2" >
+        <
+        h5 > Company < /h5> <
+        ul >
+        <
+        li className = "nav-item" >
+        <
+        a className = ""
+        href = "https://www.devlabsstore.tech/" >
+        Home <
+        /a> <
+        /li> <
+        li className = "nav-item" >
+        <
+        a className = ""
+        href = "https://www.devlabsstore.tech/about" >
+        About Us <
+        /a> <
+        /li> <
+        li className = "nav-item" >
+        <
+        a className = ""
+        href = "https://www.devlabsstore.tech/bookmark" >
+        Bookmark <
+        /a> <
+        /li> <
+        li className = "nav-item" >
+        <
+        a className = ""
+        href = "https://www.linkedin.com/in/himanshunarware/" >
+        Contact <
+        /a> <
+        /li> <
+        li className = "nav-item" >
+        <
+        a className = ""
+        href = "https://www.devlabsstore.tech/open-source" >
+        Open Source <
+        /a> <
+        /li> <
+        /ul> <
+        /div> <
+        div className = "col-md-6 col-lg-3 col-12 ft-2" >
+        <
+        h5 > Quick Links < /h5> <
+        ul >
 
-                <li className='nav-item'>                  <Link className='' to='/remote-jobs'>
-                    Remote Jobs
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link className='' to='/ai'>
-                    AI
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link className='' to='/movies-series'>
-                    Movie/Series
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link className='' to='/extension'>
-                    Extension
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link className='' to='/ui-design'>
+        <
+        li className = 'nav-item' > < Link className = ''
+        to = '/remote-jobs' >
+        Remote Jobs <
+        /Link> <
+        /li> <
+        li className = 'nav-item' >
+        <
+        Link className = ''
+        to = '/ai' >
+        AI <
+        /Link> <
+        /li> <
+        li className = 'nav-item' >
+        <
+        Link className = ''
+        to = '/movies-series' >
+        Movie / Series <
+        /Link> <
+        /li> <
+        li className = 'nav-item' >
+        <
+        Link className = ''
+        to = '/extension' >
+        Extension <
+        /Link> <
+        /li> <
+        li className = 'nav-item' >
+        <
+        Link className = ''
+        to = '/ui-design' >
 
-                <li className="nav-item">
-                  <Link className="" to="/remote-jobs">
-                    Remote Jobs
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="" to="/ai">
-                    AI
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="" to="/movies-series">
-                    Movie/Series
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="" to="/extension">
-                    Extension
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="" to="/ui-design">
+        <
+        li className = "nav-item" >
+        <
+        Link className = ""
+        to = "/remote-jobs" >
+        Remote Jobs <
+        /Link> <
+        /li> <
+        li className = "nav-item" >
+        <
+        Link className = ""
+        to = "/ai" >
+        AI <
+        /Link> <
+        /li> <
+        li className = "nav-item" >
+        <
+        Link className = ""
+        to = "/movies-series" >
+        Movie / Series <
+        /Link> <
+        /li> <
+        li className = "nav-item" >
+        <
+        Link className = ""
+        to = "/extension" >
+        Extension <
+        /Link> <
+        /li> <
+        li className = "nav-item" >
+        <
+        Link className = ""
+        to = "/ui-design" >
 
-                    UI Design
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-6 col-lg-2 col-12 ft-4">
-              <h5>Explore</h5>
-              <ul>
+        UI Design <
+        /Link> <
+        /li> <
+        /ul> <
+        /div> <
+        div className = "col-md-6 col-lg-2 col-12 ft-4" >
+        <
+        h5 > Explore < /h5> <
+        ul >
 
-                <li className='nav-item'>
-                  <Link className='' to='/front-end-tools'>
-                    Frontend Tools
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link className='' to='/ethical-hacking'>
-                    Ethical Hacking
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link className='' to='/coding-platform'>
-                    Coding Platforms
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link className='' to='/course-platform'>
-=
-                <li className="nav-item">
-                  <Link className="" to="/front-end-tools">
-                    Frontend Tools
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="" to="/ethical-hacking">
-                    Ethical Hacking
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="" to="/coding-platform">
-                    Coding Platforms
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="" to="/courses-platform">
->>
-                    Course Platforms
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='Last-footer'>
-        <div className='container'>
-          <div className='row justify-content-center'>
-            <div className='col-md-6 col-12 ft-1'>
-              <p className='text-center fs-6'>
-                COPYRIGHT © Devlaps 2024 | <br /> MAINTAINED BY HIMANSHU NARWARE
-              </p>
-              <div className='footer-icon mb-2'>
-                <a
-                  href='https://www.linkedin.com/in/himanshunarware/'
-                  rel='noreferrer'
-                  target='_blank'
-                >
-                  <BsLinkedin className='text-white mx-1 fs-4' />
-                </a>
-                <a
-                  href='https://twitter.com/N_Himanshu_/'
-                  rel='noreferrer'
-                  target='_blank'
-                >
-                  <i className='fa-brands fa-square-x-twitter text-white mx-1 fs-4'></i>
+        <
+        li className = 'nav-item' >
+        <
+        Link className = ''
+        to = '/front-end-tools' >
+        Frontend Tools <
+        /Link> <
+        /li> <
+        li className = 'nav-item' >
+        <
+        Link className = ''
+        to = '/ethical-hacking' >
+        Ethical Hacking <
+        /Link> <
+        /li> <
+        li className = 'nav-item' >
+        <
+        Link className = ''
+        to = '/coding-platform' >
+        Coding Platforms <
+        /Link> <
+        /li> <
+        li className = 'nav-item' >
+        <
+        Link className = ''
+        to = '/course-platform' > = <
+        li className = "nav-item" >
+        <
+        Link className = ""
+        to = "/front-end-tools" >
+        Frontend Tools <
+        /Link> <
+        /li> <
+        li className = "nav-item" >
+        <
+        Link className = ""
+        to = "/ethical-hacking" >
+        Ethical Hacking <
+        /Link> <
+        /li> <
+        li className = "nav-item" >
+        <
+        Link className = ""
+        to = "/coding-platform" >
+        Coding Platforms <
+        /Link> <
+        /li> <
+        li className = "nav-item" >
+        <
+        Link className = ""
+        to = "/courses-platform" >
+        >>
+        Course Platforms <
+        /Link> <
+        /li> <
+        /ul> <
+        /div> <
+        /div> <
+        /div> <
+        /div> <
+        div className = 'Last-footer' >
+        <
+        div className = 'container' >
+        <
+        div className = 'row justify-content-center' >
+        <
+        div className = 'col-md-6 col-12 ft-1' >
+        <
+        p className = 'text-center fs-6' >
+        COPYRIGHT© Devlaps 2024 | < br / > MAINTAINED BY HIMANSHU NARWARE <
+        /p> <
+        div className = 'footer-icon mb-2' >
+        <
+        a href = 'https://www.linkedin.com/in/himanshunarware/'
+        rel = 'noreferrer'
+        target = '_blank' >
+        <
+        BsLinkedin className = 'text-white mx-1 fs-4' / >
+        <
+        /a> <
+        a href = 'https://twitter.com/N_Himanshu_/'
+        rel = 'noreferrer'
+        target = '_blank' >
+        <
+        i className = 'fa-brands fa-square-x-twitter text-white mx-1 fs-4' > < /i>
 
-      <div className="Last-footer">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-6 col-12 ft-1">
-              <p className="text-center fs-6">
-                COPYRIGHT © Devlaps 2024 | <br /> MAINTAINED BY HIMANSHU NARWARE
-              </p>
-              <div className="footer-icon mb-2">
-                <a href="https://www.linkedin.com/in/himanshunarware/" rel="noreferrer" target="_blank">
-                  <BsLinkedin className="text-white mx-1 fs-4" />
-                </a>
-                <a href="https://twitter.com/N_Himanshu_/" rel="noreferrer" target="_blank">
-                  <FaTwitter className="text-white mx-1 fs-4" />
-                </a>
-                <a href="mailto:himanshunarware77@gmail.com" rel="noreferrer">
-                  <SiGmail className="text-white mx-1 fs-4" />
-                </a>
-                <a href="https://github.com/HimanshuNarware/Devlabs" rel="noreferrer" target="_blank">
-                  <FaGithubSquare className="text-white mx-1 fs-4" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+        <
+        div className = "Last-footer" >
+        <
+        div className = "container" >
+        <
+        div className = "row justify-content-center" >
+        <
+        div className = "col-md-6 col-12 ft-1" >
+        <
+        p className = "text-center fs-6" >
+        COPYRIGHT© Devlaps 2024 | < br / > MAINTAINED BY HIMANSHU NARWARE <
+        /p> <
+        div className = "footer-icon mb-2" >
+        <
+        a href = "https://www.linkedin.com/in/himanshunarware/"
+        rel = "noreferrer"
+        target = "_blank" >
+        <
+        BsLinkedin className = "text-white mx-1 fs-4" / >
+        <
+        /a> <
+        a href = "https://twitter.com/N_Himanshu_/"
+        rel = "noreferrer"
+        target = "_blank" >
+        <
+        FaTwitter className = "text-white mx-1 fs-4" / >
+        <
+        /a> <
+        a href = "mailto:himanshunarware77@gmail.com"
+        rel = "noreferrer" >
+        <
+        SiGmail className = "text-white mx-1 fs-4" / >
+        <
+        /a> <
+        a href = "https://github.com/HimanshuNarware/Devlabs"
+        rel = "noreferrer"
+        target = "_blank" >
+        <
+        FaGithubSquare className = "text-white mx-1 fs-4" / >
+        <
+        /a> <
+        /div> <
+        /div> <
+        /div> <
+        /div> <
+        /div> <
+        /footer>
+    );
 }
 
 export default Footer;
