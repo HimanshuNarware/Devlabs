08990b322a81d9991e828d4eed4d2238c9da9c8b
Sailaja Adapa <adapasailaja17@gmail.com>
2024-06-11 08:45:31 +0530

    Added Icons To Navbar

--- frontend/src/Component/Rateus.js
+++ frontend/src/Component/Rateus.js
@@ -4,7 +4,6 @@ import "./../style/Rateus.css";
 function RateUsComponent({ previousContent }) {
   const [rating, setRating] = useState(0);
   const [feedback, setFeedback] = useState("");
-  const [showModal, setShowModal] = useState(false);
 
   const handleStarClick = (star) => {
     setRating(star);
@@ -15,40 +14,40 @@ function RateUsComponent({ previousContent }) {
   };
 
   const handleSubmit = () => {
-    // Reset the state after submitting
-    setRating(0);
-    setFeedback("");
-    setShowModal(false); // Hide the modal after submitting
+    // Check if both rating and feedback are provided
+    if (rating !== 0 && feedback.trim() !== "") {
+      // Reset the state after submitting
+      setRating(0);
+      setFeedback("");
+    } 
   };
 
   return (
-    <>
-      <div className="rate-us-page">
-        <div className="rate-us-container">
-          <h2 className="rate-us-heading">Rate Our Website</h2>
-          <div className="star-rating">
-            {[1, 2, 3, 4, 5].map((star) => (
-              <span
-                key={star}
-                className={star <= rating ? "star filled" : "star"}
-                onClick={() => handleStarClick(star)}
-              >
-                ★
-              </span>
-            ))}
-          </div>
-          <textarea
-            className="feedback-textarea"
-            placeholder="Write your feedback here..."
-            value={feedback}
-            onChange={handleFeedbackChange}
-          ></textarea>
-          <button className="submit-button" onClick={() => setShowModal(true)}>
-            Submit
-          </button>
+    <div className="rate-us-page">
+      <div className="rate-us-container">
+        <h2 className="rate-us-heading">Rate Our Website</h2>
+        <div className="star-rating">
+          {[1, 2, 3, 4, 5].map((star) => (
+            <span
+              key={star}
+              className={star <= rating ? "star filled" : "star"}
+              onClick={() => handleStarClick(star)}
+            >
+              ★
+            </span>
+          ))}
         </div>
+        <textarea
+          className="feedback-textarea"
+          placeholder="Write your feedback here..."
+          value={feedback}
+          onChange={handleFeedbackChange}
+        ></textarea>
+        <button className="submit-button" onClick={handleSubmit}>
+          Submit
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
