b23eac6b335de8f686742f5535787988094a2847
Braj Narayan Awasthi <3083brajnarayanawasthi@gmail.com>
2024-06-12 09:14:42 +0530

    updated courses platform page

--- frontend/src/pages/CoursesPlatform.jsx
+++ frontend/src/pages/CoursesPlatform.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import "../style/CoursesPlatform.css";
 const tools = [
   { name: "Striver A2Z DSA Course", description: "A DSA Course from takeuforward by striver", link: "https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2/", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkJNpQ_lbzLqCsgQb1dfTux4hAEPTHw2yZs5uIjkfGash21h4_875srglEmLgGhXtoDa0&usqp=CAU" },
-{ name:"KG Coding webdev Course",description:"Web Development courses",linl:"https://www.kgcoding.in/courses/JavaScript-Youtube-Course-6528e3a1e4b0f4457f0f1baa",image:"https://d502jbuhuh9wk.cloudfront.net/courses/6528e3a1e4b0f4457f0f1baa/6528e3a1e4b0f4457f0f1baa_scaled_cover.jpg?v=1"
-
-},];
+];
 
 const CoursesPlatform = () => {
   return (
