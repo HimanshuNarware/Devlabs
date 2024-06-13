cb043ff6a5b4b9709d6b2dfd7c27f8556c37d69f
Braj Narayan Awasthi <3083brajnarayanawasthi@gmail.com>
2024-06-13 06:10:44 +0530

    Update CoursesPlatform.jsx

--- frontend/src/pages/CoursesPlatform.jsx
+++ frontend/src/pages/CoursesPlatform.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import "../style/CoursesPlatform.css";
 const tools = [
   { name: "Striver A2Z DSA Course", description: "A DSA Course from takeuforward by striver", link: "https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2/", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkJNpQ_lbzLqCsgQb1dfTux4hAEPTHw2yZs5uIjkfGash21h4_875srglEmLgGhXtoDa0&usqp=CAU" },
