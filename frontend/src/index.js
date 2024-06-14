95f3ad537cd8a98eb4d5d4536ba21ea8fc17b800
Rohitha Pudu <rohithapudu@gmail.com>
2024-06-14 15:15:07 +0530

    remove bugs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -39,8 +39,6 @@ root.render(
   <Provider store={store}>p
     <BrowserRouter> {/* Ensure BrowserRouter is only here */}
       <App /> 
-    <BrowserRouter>
-      <App />
 
       <ToastContainer />
     </BrowserRouter>
