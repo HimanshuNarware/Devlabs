698e2c08ca9c860840c5ae012d7b96334ae21a0c
kiranpande0 <kpande026@gmail.com>
2024-06-15 21:14:47 +0530

    Minor_changes

--- frontend/src/Component/Footer.js
+++ frontend/src/Component/Footer.js
@@ -42,30 +42,8 @@ function Footer() {
                   <a href="https://www.linkedin.com/in/himanshunarware/">Contact</a>
                 </li>
                 <li className="nav-item">
-
                   <a href="https://www.devlabsstore.tech/open-source">Open Source</a>
-
-                  <a className="" href="https://www.devlabsstore.tech/open-source">
-                    Open Source
-                  </a>
                   </li>
-                <li className='nav-item'>
-                  <a href='https://www.devlabsstore.tech/'>Home</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='https://www.devlabsstore.tech/about'>About Us</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='https://www.devlabsstore.tech/bookmark'>Bookmark</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='https://www.linkedin.com/in/himanshunarware/'>Contact</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='https://www.devlabsstore.tech/open-source'>Open Source</a>
-
-
-                </li>
               </ul>
             </div>
             <div className="col-md-6 col-lg-3 col-12 ft-2">
@@ -85,29 +63,6 @@ function Footer() {
                 </li>
                 <li className="nav-item">
                   <a href="/ui-design">UI Design</a>
-                  <Link className="" to="/ui-design">
-                    UI Design
-                  </Link>
-                  <a className="" href="/ui-design">
-                    UI Design
-                  </a>
-                </li>
-
-                <li className='nav-item'>
-                  <a href='/remote-jobs'>Remote Jobs</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='/ai'>AI</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='/movies-series'>Movie/Series</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='/extension'>Extension</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='/ui-design'>UI Design</a>
-
                 </li>
               </ul>
             </div>
@@ -124,28 +79,7 @@ function Footer() {
                   <a href="/coding-platform">Coding Platforms</a>
                 </li>
                 <li className="nav-item">
-
                   <a href="/courses-platform">Course Platforms</a>
-                  <Link className="" to="/courses-platform">
-
-                  <a className="" href="/course-platform">
-
-                    Course Platforms
-                  </a>
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <a href='/front-end-tools'>Frontend Tools</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='/ethical-hacking'>Ethical Hacking</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='/coding-platform'>Coding Platforms</a>
-                </li>
-                <li className='nav-item'>
-                  <a href='/courses-platform'>Course Platforms</a>
-
                 </li>
               </ul>
             </div>
