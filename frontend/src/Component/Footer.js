980f7844bec42225b5ef8465b0769f8b12e8c7b2
Sailaja Adapa <161804629+sailaja-adapa@users.noreply.github.com>
2024-07-03 15:33:52 +0530

    Updated Footer.js

--- frontend/src/Component/Footer.js
+++ frontend/src/Component/Footer.js
@@ -1,23 +1,25 @@
 import React from "react";
-import logo from "../image/logo.png";
+import logo from "../image/logo1.png";
 import "../style/Footer.css";
-import { FaGithubSquare, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 import { SiGmail } from "react-icons/si";
 import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer>
-      <div className="Footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 col-lg-4 col-12 ft-1">
-              <a href="https://www.devlabsstore.tech/">
-                <img width={209} src={logo} alt="devlaps" />
+      <div className='Footer'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6 col-lg-4 col-12 ft-1'>
+              <a href='https://www.devlabsstore.tech/'>
+                <img width={209} src={logo} alt='devlabs' />
               </a>
               <h5>
                 Facing an issue or just <br />
-                enjoying the site?
+                enjoying the site?{" "}
               </h5>
               <button>
                 <Link to="/review">Tell us now</Link>
@@ -26,21 +28,9 @@ function Footer() {
             <div className="col-md-6 col-lg-3 col-12 ft-2">
               <h5>Company</h5>
               <ul>
+                
                 <li className="nav-item">
-                  <a href="https://www.devlabsstore.tech/">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a href="https://www.devlabsstore.tech/about">About Us</a>
-                </li>
-                <li className="nav-item">
-                  <a href="https://www.devlabsstore.tech/bookmark">Bookmark</a>
-                </li>
-                <li className="nav-item">
-                  <a href="https://www.linkedin.com/in/himanshunarware/">Contact</a>
-                </li>
-                <li className="nav-item">
-                  <a href="https://www.devlabsstore.tech/open-source">Open Source</a>
-                </li>
+                  </li>
               </ul>
             </div>
             <div className="col-md-6 col-lg-3 col-12 ft-2">
@@ -88,15 +78,14 @@ function Footer() {
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-md-6 col-12 ft-1">
-              <div className="text-center fs-6 my-3">
+              <div className="text-center fs-6 my-3 ">
                 COPYRIGHT © Devlaps 2024 | <br /> MAINTAINED BY HIMANSHU NARWARE
               </div>
-              <div className="footer-icons mb-2">
+              <div className="footer-icon mb-2">
                 <a
                   href="https://www.linkedin.com/in/himanshunarware/"
                   rel="noreferrer"
                   target="_blank"
-                  className="footer-icon"
                 >
                   <FaLinkedin className="text-white mx-1 fs-4" />
                 </a>
@@ -104,14 +93,12 @@ function Footer() {
                   href="https://twitter.com/N_Himanshu_/"
                   rel="noreferrer"
                   target="_blank"
-                  className="footer-icon"
                 >
-                  <FaTwitter className="text-white mx-1 fs-4" />
+                  <FaSquareXTwitter className="text-white mx-1 fs-4" />
                 </a>
                 <a
                   href="mailto:himanshunarware77@gmail.com"
                   rel="noreferrer"
-                  className="footer-icon"
                 >
                   <SiGmail className="text-white mx-1 fs-4" />
                 </a>
@@ -119,7 +106,6 @@ function Footer() {
                   href="https://github.com/HimanshuNarware/Devlabs"
                   rel="noreferrer"
                   target="_blank"
-                  className="footer-icon"
                 >
                   <FaGithubSquare className="text-white mx-1 fs-4" />
                 </a>
