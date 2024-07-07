6ab8e702c55d2bb4409a151eaa59568ff9eca2b4
Utsavladia <utsavladia@gmail.com>
2024-07-07 10:03:32 +0530

    Made responsive

--- frontend/src/Component/Faq.js
+++ frontend/src/Component/Faq.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../style/faq.css";
 import {
   Accordion,
@@ -7,12 +7,10 @@ import {
   AccordionItemButton,
   AccordionItemPanel,
   AccordionItemState,
-} from 'react-accessible-accordion';
-import { MdOutlineArrowDropDown } from 'react-icons/md';
-import data from './accordion';
-import 'react-accessible-accordion/dist/fancy-example.css';
-
-import { useSearchParams } from 'react-router-dom';
+} from "react-accessible-accordion";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import data from "./accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
 
 export default function Faq() {
   let [index, setIndex] = useState(0);
@@ -25,94 +23,43 @@ export default function Faq() {
           <span className="faq__header-text">Frequently Asked Questions</span>
           <Accordion
             className="faq__border-none faq__mt-8"
-
-
-export default function Faq() {
-  let [index, setIndex] = useState(0);
-
-  return (
-    <section className="faq">
-      <div className="faq1">
-        <div className="yet-another-custom-class">
-          <span className="text">Our Value</span>
-          <span className="text-white">
-            MyStory - Next.js Blog/Story telling Template
-          </span>
-          <span className="text-gray-500-text-sm">
-            MyStory aims to provide a platform where individuals can share their
-            stories without the pressure of revealing their identities.
-            It&apos;s a safe space for expression and connection.
-          </span>
-          <Accordion
-            className="border-none mt-8"
-
             allowMultipleExpanded={false}
             preExpanded={[0]}
           >
             {data.map((item, i) => {
               return (
                 <AccordionItem
-
-                  className={`faq__border-2 faq__border-gray faq__rounded-lg faq__overflow-hidden faq__mb-5 ${index === i ? 'faq__accordion-open' : 'faq__accordion-close'}`}
-
-                  className={`border-2 border-gray-400 rounded-lg overflow-hidden mb-5 ${index === i ? 'open' : 'close'}`}
-
+                  className={`faq__border-2 faq__border-gray faq__rounded-lg faq__overflow-hidden faq__mb-5 ${
+                    index === i ? "faq__accordion-open" : "faq__accordion-close"
+                  }`}
                   key={i}
                   uuid={i}
                 >
                   <AccordionItemHeading>
-
                     <AccordionItemButton className="faq__accordion-item">
                       <AccordionItemState>
                         {({ expanded }) =>
                           expanded
-                            ? setClassName('expanded')
-                            : setClassName('collapsed')
+                            ? setClassName("expanded")
+                            : setClassName("collapsed")
                         }
                       </AccordionItemState>
-                      <div className="faq__accordion-button">
-                        <div className="faq__accordion-icon">
-                          {item.icon}
-                        </div>
-                        <span className="font-bold">
-                          {item.heading}
-                        </span>
-                        <div className="faq__accordion-container" onClick={() => {
-                              setIndex(i);
-                            }}>
-
-                    <AccordionItemButton className="custom-flex-container">
-                      <AccordionItemState>
-                        {({ expanded }) => (
-                          <div className={expanded ? 'expanded' : 'collapsed'} />
-                        )}
-                      </AccordionItemState>
-                      <div className="custom-flex-container1">
-                        <div className="custom-flex-container2">
-                          {item.icon}
-                        </div>
-                        <span className='font-bold'>
-                          {item.heading}
-                        </span>
-                        <div className="custom-container2" onClick={() => {
-                          setIndex(i);
-                        }}>
-
-                          <MdOutlineArrowDropDown
-                            style={{ color: "white" }}
-                            size={20}
-                          />
-                        </div>
+                      <div
+                        className="faq__accordion-button"
+                        onClick={() => setIndex(i)}
+                      >
+                        <div className="faq__accordion-icon">{item.icon}</div>
+                        <span className="font-bold">{item.heading}</span>
+                        <MdOutlineArrowDropDown
+                          style={{ color: "white" }}
+                          size={20}
+                        />
                       </div>
                     </AccordionItemButton>
                   </AccordionItemHeading>
                   <AccordionItemPanel>
-
                     <p className="faq__accordion-detail">
-
-                    <p className="custom-text1">
-
-                      {item.detail}
+                      <p className="custom-text1">{item.detail}</p>
                     </p>
                   </AccordionItemPanel>
                 </AccordionItem>
