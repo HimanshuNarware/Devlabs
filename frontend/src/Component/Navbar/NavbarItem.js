a4bcd52a4b35af1b38064dddf07706602703e32f
Sailaja Adapa <161804629+sailaja-adapa@users.noreply.github.com>
2024-07-21 11:09:12 +0530

    Update NavbarItem.js

--- frontend/src/Component/Navbar/NavbarItem.js
+++ frontend/src/Component/Navbar/NavbarItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 function NavbarItem({ description, to }) {
   return (
     <Link to={to} className="Link nav-link active" aria-current="page">
