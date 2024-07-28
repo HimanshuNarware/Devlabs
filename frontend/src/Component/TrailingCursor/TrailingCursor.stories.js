426c13d82f15955f3d955045b17f8ba1ca6c6dfb
~Chiluka Akshitha <120377576+AKSHITHA-CHILUKA@users.noreply.github.com>
2024-07-28 10:18:55 +0530

    Create TrailingCursor.stories.js

---
+++ frontend/src/Component/TrailingCursor/TrailingCursor.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TrailingCursor from './TrailingCursor';
+export default {
+  title: 'Components/TrailingCursor',
+  component: TrailingCursor,
+};
+const Template = (args) => <TrailingCursor {...args} />;
+export const Default = Template.bind({});
+Default.args = {};
