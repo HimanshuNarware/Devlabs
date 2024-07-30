1bc166c0b05819f3ce78a9e7f07ba5bf916816bb
ANKeshri <ankeshri2003@gmail.com>
2024-07-30 18:00:37 +0530

    add nTask new collaboration tool

--- frontend/src/pages/Collaboration.jsx
+++ frontend/src/pages/Collaboration.jsx
@@ -127,6 +127,13 @@ const movies = [
     image: "https://www.ntaskmanager.com/wp-content/uploads/2022/05/nTask-Logo-Vertical.svg",
     releaseDate: "2015-07-01"
   },  
+  {
+    title: "nTask",
+    category: "Collaboration Tools",
+    description: "nTask is a project management and collaboration tool that helps teams manage tasks, projects, and workflows efficiently.",
+    image: "https://www.ntaskmanager.com/wp-content/uploads/2022/05/nTask-Logo-Vertical.svg",
+    releaseDate: "2015-07-01"
+  },  
 ];
 const Collaboration = () => {
   return (
