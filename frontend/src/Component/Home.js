b675cd487f8862e01f43284d7463e7e763694e4e
Utsavladia <utsavladia@gmail.com>
2024-08-07 10:42:39 +0530

    Addded new too to convert font online

--- frontend/src/Component/Home.js
+++ frontend/src/Component/Home.js
@@ -286,7 +286,6 @@ function Home(props) {
 
   return (
     <SkeletonTheme>
-      <div>
         <div className="hero">
           <div className="hero-text">
             <div id="hero" className="hero-container">
