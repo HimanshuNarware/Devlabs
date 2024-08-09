fad2975db704db629d06917a1ff2dd852fd651f4
Sailaja Adapa <161804629+sailaja-adapa@users.noreply.github.com>
2024-08-09 17:34:43 +0530

    Update Rateus.js

--- frontend/src/Component/Rateus.js
+++ frontend/src/Component/Rateus.js
@@ -2,14 +2,12 @@ import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./../style/Rateus.css";
-
 function RateUsComponent({ previousContent }) {
   const [rating, setRating] = useState(0);
   const [feedback, setFeedback] = useState("");
   const handleFeedbackChange = (e) => {
     setFeedback(e.target.value);
   };
-  
   const handleEmojiClick = (emojiIndex) => {
     setRating(emojiIndex);
   };
