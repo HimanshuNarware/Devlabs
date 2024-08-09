eea8ef2674c2f84b2f455ceed78d44b7ea71267e
Sailaja Adapa <161804629+sailaja-adapa@users.noreply.github.com>
2024-08-09 19:37:52 +0530

    Update Sidebar.js

--- frontend/src/Component/Sidebar/Sidebar.js
+++ frontend/src/Component/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BsBookmark } from "react-icons/bs";
-import { AiFillHome } from "react-icons/ai";
+import { AiFillHome } from "react-icons/ai";   
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
